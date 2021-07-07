e81fa6fa1814f0699cd2ca42e69049664b291d9b
Jordan Siemens <jordan.siemens@blacklane.com>
2021-07-07 11:50:15 +0200

    [IA-66] Add weather API

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'json'
+require 'open-uri'
+require "sinatra/reloader"
 
-get '/' do 
-    'Hello World'
+get '/' do
+  erb :index
+end
+
+post '/weather' do
+  # "#{params["city"]}"
+  @api = "http://api.openweathermap.org/data/2.5/weather?q=#{params["city"]}&units=metric&appid=a64896bb4676c4331e621f1940dc623d
+"
+  @weather_serialized = URI.open(@api).read
+
+  @weather = JSON.parse(@weather_serialized)
+
+  erb :weather
 end
