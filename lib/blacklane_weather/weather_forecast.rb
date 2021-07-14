5839d00653fa7abeed3db64d0b39f2fe54872e7a
jordannadroj <jordan.siemens@blacklane.com>
2021-07-14 11:05:24 +0200

    [IA-66] Replace 'URI.open' with 'Net:HTTP' to read api to eliminate security risk

--- lib/blacklane_weather/weather_forecast.rb
+++ lib/blacklane_weather/weather_forecast.rb
@@ -3,6 +3,7 @@
 require "json"
 require "open-uri"
 require "./lib/errors/invalid_city_error"
+require "net/http"
 require_relative "temperature_data"
 
 class WeatherForecast
@@ -16,14 +17,14 @@ class WeatherForecast
     api = "http://api.openweathermap.org/data/2.5/weather?q=#{@city}&units=metric&appid=#{ENV['OPENWEATHER_API_KEY']}"
     # api = "http://api.openweathermap.org/data/2.5/weather?q=#{@city}&units=metric&appid=a64896bb4676c4331e621f1940dc623d"
 
-    weather_serialized = URI.open(api).read
-
+    uri = URI(api)
+    weather_serialized = Net::HTTP.get(uri)
     weather_data = JSON.parse(weather_serialized)
     temperature_data(weather_data)
     #  better to return an object
     # define our own structure and provide only what is relevant to expose
     # make a new blacklane_weather instance with specific data
-  rescue OpenURI::HTTPError => e
+  rescue NoMethodError => e
     raise Errors::InvalidCityError.new(e, @city)
   end
 
