1ba3fcf4d9146528bd5c55e9830827ee14272f9c
jordannadroj <jordan.siemens@blacklane.com>
2021-07-21 17:10:48 +0200

    [IA-67] reconfigure API to translate city to gps coordinates

--- lib/blacklane_weather/weather_forecast.rb
+++ lib/blacklane_weather/weather_forecast.rb
@@ -15,17 +15,13 @@ module BlacklaneWeather
     end
 
     def weather_call
-      api = "http://api.openweathermap.org/data/2.5/weather?q=#{@city}&units=metric&appid=#{ENV['OPENWEATHER_API_KEY']}"
+      coordinates = location_coordinates
+      api = "http://api.openweathermap.org/data/2.5/weather?lat=#{coordinates[:lat]}&lon=#{coordinates[:lon]}&units=metric&appid=#{ENV['OPENWEATHER_API_KEY']}"
 
       uri = URI(api)
       http_response = Net::HTTP.get_response(uri)
-
-      if http_response.is_a?(Net::HTTPSuccess)
-        weather_data = JSON.parse(http_response.body)
-        temperature_data(weather_data)
-      else
-        raise Errors::InvalidCityError, @city
-      end
+      weather_data = JSON.parse(http_response.body)
+      temperature_data(weather_data)
     end
 
     private
@@ -33,6 +29,20 @@ module BlacklaneWeather
     def temperature_data(weather_data)
       TemperatureData.new(weather_data["main"]["temp"], weather_data["main"]["feels_like"],
                           weather_data["main"]["temp_min"], weather_data["main"]["temp_max"])
+    end
+
+    def location_coordinates
+      geolocation = "http://api.openweathermap.org/geo/1.0/direct?q=#{@city}&limit=1&appid=#{ENV['OPENWEATHER_API_KEY']}"
+      geo_uri = URI(geolocation)
+      geo_http = Net::HTTP.get_response(geo_uri)
+      geo_json = JSON.parse(geo_http.body)
+      if geo_json.empty?
+        raise Errors::InvalidCityError, @city
+      else
+        latitude = geo_json[0]["lat"]
+        longitude = geo_json[0]["lon"]
+        { lat: latitude, lon: longitude }
+      end
     end
   end
 end
--- spec/weather_forecast_spec.rb
+++ spec/weather_forecast_spec.rb
@@ -5,20 +5,21 @@ require "./lib/blacklane_weather/temperature_data"
 require "rspec"
 require "rack/test"
 
-RSpec.describe "The Weather Forecast class" do
+RSpec.describe BlacklaneWeather::WeatherForecast do
   # Use let to reuse Berlin blacklane_weather instance
   let(:berlin_city) { "Berlin" }
+  let(:berlin_coordinates) { { lat: 52.5244, lon: 13.4105 } }
   let(:invalid_city) { "Invalid" }
 
   let(:berlin_instance) { BlacklaneWeather::WeatherForecast.new(berlin_city) }
 
   let(:invalid_instance) { BlacklaneWeather::WeatherForecast.new(invalid_city) }
 
-  let(:api) {
-    "http://api.openweathermap.org/data/2.5/weather?q=#{berlin_city}&units=metric&appid=#{ENV['OPENWEATHER_API_KEY']}"
+  let(:weather_api) {
+    "http://api.openweathermap.org/data/2.5/weather?lat=#{berlin_coordinates[:lat]}&lon=#{berlin_coordinates[:lon]}&units=metric&appid=#{ENV['OPENWEATHER_API_KEY']}"
   }
 
-  let(:incorrect_api) {
+  let(:invalid_weather_api) {
     "http://api.openweathermap.org/data/2.5/weather?q=#{invalid_city}&units=metric&appid=#{ENV['OPENWEATHER_API_KEY']}"
   }
 
@@ -29,10 +30,19 @@ RSpec.describe "The Weather Forecast class" do
       "weather": [{ "id": 803, "main": "Clouds", "description": "broken clouds", "icon": "04d" }], "base": "stations", "main": { "temp": 27.35, "feels_like": 28.13, "temp_min": 25.14, "temp_max": 28.46, "pressure": 1004, "humidity": 55 }, "visibility": 10_000, "wind": { "speed": 3.58, "deg": 295, "gust": 6.26 }, "clouds": { "all": 75 }, "dt": 1_626_358_147, "sys": { "type": 2, "id": 2_011_538, "country": "DE", "sunrise": 1_626_318_094, "sunset": 1_626_376_960 }, "timezone": 7200, "id": 2_950_159, "name": "Berlin", "cod": 200 }.to_json
   }
 
-  let(:stub) { stub_request(:get, api).to_return(status: status, body: body) }
+  let(:stub) { stub_request(:get, weather_api).to_return(status: status, body: body) }
+
+  let(:coordinates_api) { "http://api.openweathermap.org/geo/1.0/direct?q=#{berlin_city}&limit=1&appid=#{ENV['OPENWEATHER_API_KEY']}" }
+
+  let(:invalid_coordinates_api) { "http://api.openweathermap.org/geo/1.0/direct?q=#{invalid_city}&limit=1&appid=#{ENV['OPENWEATHER_API_KEY']}" }
+
+  let(:coordinates_body) { [{"name":"Berlin","local_names":{"af":"Berlyn","ar":"برلين","ascii":"Berlin","bg":"Берлин","ca":"Berlín","da":"Berlin","de":"Berlin","el":"Βερολίνο","en":"Berlin","eu":"Berlin","fa":"برلین","feature_name":"Berlin","fi":"Berliini","fr":"Berlin","gl":"Berlín","he":"ברלין","hr":"Berlin","hu":"Berlin","id":"Berlin","it":"Berlino","ja":"ベルリン","la":"Berolinum","lt":"Berlynas","mk":"Берлин","nl":"Berlijn","no":"Berlin","pl":"Berlin","pt":"Berlim","ro":"Berlin","ru":"Берлин","sk":"Berlín","sl":"Berlin","sr":"Берлин","th":"เบอร์ลิน","tr":"Berlin","vi":"Berlin"},"lat":52.5244,"lon":13.4105,"country":"DE"}].to_json }
+
+  let(:coordinates_stub) { stub_request(:get, coordinates_api).to_return(status: status, body: coordinates_body) }
 
   before do |e|
     unless e.metadata[:skip_before]
+      coordinates_stub
       stub
     end
   end
@@ -62,7 +72,8 @@ RSpec.describe "The Weather Forecast class" do
   end
 
   it "raises an error with invalid city", :skip_before do
-    stub_request(:get, incorrect_api).to_return(status: 404, body: '{"cod":"404","message":"city not found"}')
+    stub_request(:get, invalid_coordinates_api).to_return(status: status, body: '[]')
+    stub_request(:get, invalid_weather_api).to_return(status: 404, body: '{"cod":"404","message":"city not found"}')
 
     expect { invalid_instance.weather_call }.to raise_error(Errors::InvalidCityError)
   end
